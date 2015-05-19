b67f14abd4f609c9de303e5d188f7fc994fc74f3
Josh Aronoff <mbp85chem@Joshs-MBP-2.home>
2015-05-19 01:21:17 -0400

    add navigation and about page and contact page.

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
+
+
     # Examples:
     # url(r'^$', 'hellowebapp.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'$', 'collection.views.index', name='home')
-    # url(r'^admin/', include(admin.site.urls)),
+   	url(r'^$', 'collection.views.index', name='home'),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
+    url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
+    url(r'^admin/', include(admin.site.urls)),
 )
