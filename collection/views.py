5906de425384e686b18990f58ccabf41e35af38a
Josh Aronoff <mbp85chem@Joshs-MBP-2.home>
2015-05-22 00:21:03 -0400

    working with variables.

--- collection/views.py
+++ collection/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def index (request):
-	#this is your new view.
-	return render(request, 'index.html')
+	#d efining the variable
+	number = 12
+
+	# passing the variable to the view.
+	return render(request, 'index.html', {
+		'number':number,
+		})
